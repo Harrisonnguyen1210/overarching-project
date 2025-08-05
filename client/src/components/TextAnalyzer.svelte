be9f040708b5391849237d359d9cfd8642f87bbd
Hung Nguyen <giahungftu2@gmail.com>
2025-08-05 22:43:06 +0300

    Client-Side scalability

---
+++ client/src/components/TextAnalyzer.svelte
@@ -0,0 +1,18 @@
+<script>
+    let text = $state("");
+    let analyzedText = $state("");
+    let charCount = $derived(analyzedText.length);
+    let ifCount = $derived((analyzedText.match(/if/g) || []).length);
+
+    const analyzeText = () => {
+        analyzedText = text;
+    };
+</script>
+
+<textarea bind:value={text}></textarea>
+<button onclick={analyzeText}>Submit</button>
+
+{#if charCount > 0}
+    <p>Characters: {charCount}</p>
+    <p>ifs: {ifCount}</p>
+{/if}
