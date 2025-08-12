8f069b1f1ed0198eed8bf84a4900406af33bf326
Hung Nguyen <giahungftu2@gmail.com>
2025-08-12 23:08:33 +0300

    Step 9

---
+++ client/src/components/ExercisePage.svelte
@@ -0,0 +1,18 @@
+<script>
+    const { exerciseId } = $props();
+
+    import { useUserState } from "../states/userState.svelte.js";
+    import ExerciseDetail from "./ExerciseDetail.svelte";
+    import TextAnalyzer from "./TextAnalyzer.svelte";
+    import UserInfo from "../components/auth/UserInfo.svelte";
+    const userState = useUserState();
+</script>
+
+<UserInfo client:only />
+<ExerciseDetail {exerciseId} />
+
+{#if userState.email}
+    <TextAnalyzer {exerciseId} />
+{:else}
+    <p>Login or register to complete exercises.</p>
+{/if}
--- client/src/components/auth/UserInfo.svelte
+++ client/src/components/auth/UserInfo.svelte
@@ -1,12 +1,15 @@
 <script>
-    import { useUserState } from "../../states/userState.svelte.js";
-    const userState = useUserState();
+  import { useUserState } from "../../states/userState.svelte.js";
+  const userState = useUserState();
 </script>
 
 {#if userState.loading}
-    <p>Loading...</p>
+  <p>Loading...</p>
 {:else if userState.email}
-    <p>{userState.email}</p>
+  <p>{userState.email}</p>
 {:else}
-    <p>No user info</p>
+  <p>
+    <a href="/auth/login">Login</a> or
+    <a href="/auth/register">Register</a>
+  </p>
 {/if}
